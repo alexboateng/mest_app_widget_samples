823a7bc0025766ac6f718f1bd5939f762b8bb955
xelahaippa <43220875+xelahaippa@users.noreply.github.com>
2022-01-31 15:09:07 +0000

    updated

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -9,13 +11,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static const textStyle = TextStyle(fontSize: 30, color: Colors.white);
+  List<String> data = [
+    "Kwame",
+    "abena",
+    "Adwoa",
+    "Afua",
+    "Kwame",
+    "abena",
+    "Adwoa",
+    "Afua",
+    "Kwame",
+    "abena",
+    "Adwoa",
+    "Afua",
+    "Kwame",
+    "abena",
+    "Adwoa",
+    "Afua"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(),
+      body: ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (BuildContext context, int pos) {
+            return _listItem(pos);
+          }),
+    );
+  }
+
+  Widget _listItem(int pos) {
+    return Container(
+      margin: EdgeInsets.only(top: 8),
+      color: Colors.red,
+      child: Text(
+        data[pos],
+        style: textStyle,
+      ),
     );
   }
 }
+
+// Container(
+//         width: MediaQuery.of(context).size.width,
+//         height: 400,
+//         color: Colors.red,
+//         child: Row(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               "Hello1",
+//               style: textStyle,
+//             ),
+//             Text(
+//               "Hello2",
+//               style: textStyle,
+//             ),
+//             Expanded(
+//               child: Container(
+//                 color: Colors.white,
+//                 child: Text(
+//                   "Hello3",
+//                   style: textStyle,
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
